64499463926dbcb36f41c1e169ce4d869df2500b
Átila Tosta <atila.tosta@gmail.com>
2019-07-24 20:09:52 -0300

    Update watson_assistant_func.py

--- src/main/python/watson_assistant_func.py
+++ src/main/python/watson_assistant_func.py
@@ -121,7 +121,7 @@ def get_logs_jupyter(num_logs, log_list, workspace_creds, log_filter=None):
                     page_limit=500,
                     cursor=current_cursor,
                     filter=log_filter
-                )
+                ).get_result()
                 min_num = min(num_logs, len(logs_response['logs']))
                 log_list.extend(logs_response['logs'][:min_num])
                 print('\r{} logs retrieved'.format(len(log_list)), end='')
